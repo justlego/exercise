9a9cc441e544997f4b49ce192ad460f600efb322
cai <746333185@qq.com>
2015-10-25 17:07:41 +0800

    exercise

---
+++ exercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  exercise
+//
+//  Created by apple on 15/10/25.
+//  Copyright © 2015年 divein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ exercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  exercise
+//
+//  Created by apple on 15/10/25.
+//  Copyright © 2015年 divein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
